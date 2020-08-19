e6d2e0131030398f87b82cf90cebd4557450f73c
ykalfa <ykalfa55@gmail.com>
2020-08-20 00:23:31 +0300

    Adding the BuildControls component AND  Outputting Burger Ingredients Dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger'
+import Burger from '../../components/Burger/Burger';
+import BuilderControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+     state = {
+         ingredients: {
+             salad: 0,
+             bacon: 0,
+             cheese: 0,
+             meat: 0
+         }
+     }
+
     render() {
         return (
             <Aux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients = {this.state.ingredients} />
+                <BuilderControls />
             </Aux>
         );
     }
