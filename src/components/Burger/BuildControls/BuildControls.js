9c4084cf3033fa95ffc2b1b514764d2b645c0fc7
ykalfa <ykalfa55@gmail.com>
2020-08-20 10:46:01 +0300

    Connecting state to BuildControls AND Removing ingredient safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import classes from './BuildControls.css';
-import BuildControl from './BuildControl/BuildControl';
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
 ];
 
-const buildControls = (props) => (
-    <div className={classes.BuildControls}>
-        {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
-        ))}
-    </div>
-)
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredienAdded(ctrl.type)}
+        removed={() => props.ingredienRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
+);
 
 export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,27 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger';
-import BuilderControls from '../../components/Burger/BuildControls/BuildControls';
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuilderControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
 
 class BurgerBuilder extends Component {
-     state = {
-         ingredients: {
-             salad: 0,
-             bacon: 0,
-             cheese: 0,
-             meat: 0
-         }
-     }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  };
 
-    render() {
-        return (
-            <Aux>
-                <Burger ingredients = {this.state.ingredients} />
-                <BuilderControls />
-            </Aux>
-        );
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    console.log(this.state.totalPrice);
+  };
+
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
     }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  render() {
+    const disableInfo = {
+      ...this.state.ingredients
+    };
+    for(let key in this.state.ingredients) {
+      disableInfo[key] = disableInfo[key] <= 0;
+    }
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuilderControls
+          ingredienAdded={this.addIngredientHandler}
+          ingredienRemoved={this.removeIngredientHandler}
+          disabled={disableInfo}
+        />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
